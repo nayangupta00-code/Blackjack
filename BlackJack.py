465ee0ed78a4e6dfc2aa2207ac38f7f8e0966dc1
Nayan <ngupta@csumb.edu>
2025-09-03 10:28:12 -0700

    Added comments and requirements

--- BlackJack.py
+++ BlackJack.py
@@ -32,6 +32,8 @@ random.shuffle(deck)
 
 # p1 = Player()
 # p2 = Player()
+
+# setting up player vs dealer
 playerCards = []
 playerCount = 0
 dealerCards = []
@@ -39,40 +41,46 @@ dealerCount = 0
 pBust = False
 dBust = False
 
+# giving player and dealer 2 cards each, alternating
 playerCards.append(deck[0])
 deck.pop(0)
-
+dealerCards.append(deck[0])
+deck.pop(0)
+playerCards.append(deck[0])
+deck.pop(0)
 dealerCards.append(deck[0])
 deck.pop(0)
 
-playerCards.append(deck[0])
-deck.pop(0)
-
-dealerCards.append(deck[0])
-deck.pop(0)
-
+# display the player two cards and add the values to the player's count
 print("Player has:")
 for card in playerCards:
     print(card.name)
     playerCount += card.value
 print("Player total count = {} \n".format(playerCount))
 
-print("Dealer shows: \n{} \n".format(dealerCards[0].name))
+# displaying the dealer's up card (2nd) and adding the dealers cards to their count
+print("Dealer shows: \n{} \n".format(dealerCards[1].name))
 for card in dealerCards:
     dealerCount += card.value
 
+# asking if player wants to draw a card.
 hit = input("Player hit? [Y/N] \n")
 
+# while a player wants to draw, 
+# remove a card from the deck and append it to the players hand
+# add the value to the players count
 while hit == "Y" or hit == "y":
     print("Player draws: {}\n".format(deck[0].name))
     playerCards.append(deck[0])
     playerCount += deck[0].value
     deck.pop(0)
     
+# show the player what cards are in hand
     print("Player has:")
     for card in playerCards:
         print(card.name)
-
+# show the players current count.
+# if the count is greater than 21, the player loses
     print("Player total count = {}\n".format(playerCount))
     if playerCount > 21:
         print("Player Busts! Game over!")
@@ -81,8 +89,16 @@ while hit == "Y" or hit == "y":
     else:
         hit = input("Player hit? [Y/N]\n")
 
-# print("Dealer showed: \n{}".format(dealerCards[0].name))
-# print("Dealer had: \n{}".format(dealerCards[1].name))
+# cont when player either does not hit or busts out
+
+# display the dealers cards
+#  first the up card and then the hidden
+print("Dealer showed: \n{}".format(dealerCards[1].name))
+print("Dealer had: \n{}".format(dealerCards[0].name))
+
+# dealer must draw while less than 17 and has not bust out
+# if true, dealer draws a card and adds to hand
+# if dealer draws too many and goes over 21, dealer has bust
 while dealerCount < 17 and not pBust:
     print("Dealer draws: {}\n".format(deck[0].name))
     dealerCards.append(deck[0])
@@ -90,11 +106,10 @@ while dealerCount < 17 and not pBust:
     deck.pop(0)
     if dealerCount > 21:
         dBust = True
-
+# all players finished drawing cards
 print("Dealer has:")
 for card in dealerCards:
     print(card.name)
-print("\n")
 print("Dealer total count: {} against player count: {}".format(dealerCount, playerCount))
 
 print 
